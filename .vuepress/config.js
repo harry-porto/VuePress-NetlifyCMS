0b16f743f69ec4c4afc60533b7f2259c14e4940e
harry-porto <harry.porto@contaazul.com>
2019-07-30 23:37:35 -0300

    removido link github

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -38,10 +38,10 @@ module.exports = {
 
 
     // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: 'harry-porto/VuePress-NetlifyCMS',
+    // repo: 'harry-porto/VuePress-NetlifyCMS',
     // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
     // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    repoLabel: 'Github',
+    // repoLabel: 'Github',
 
     // 以下为可选的编辑链接选项
 
