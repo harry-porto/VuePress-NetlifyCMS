018647fc17764371d5d8bb9ac8cb0d24d9ed43c4
harry-porto <harry.porto@contaazul.com>
2019-07-30 23:31:38 -0300

    Links alterados

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,27 +1,26 @@
 module.exports = {
-  title: 'VuePress-NetlifyCMS',
-  description: 'A VuePress Site Deployed on Netlify.com by Demo Macro',
+  title: 'Magica',
+  description: 'Design System da Conta Azul',
   dest: 'pages',
   themeConfig: {
 
     nav: [
       { text: 'Home', link: '/' },
       {
-        text: 'Projects',
+        text: 'Componentes',
         items: [
-          { text: 'URLStack', link: 'https://abc.imst.xyz' },
-          { text: 'Hexo-NetlifyCMS', link: 'https://hexocms.imst.xyz' },
-          { text: 'Gitbook-NetlifyCMS', link: 'https://gitbook.imst.xyz' }
+          { text: 'Checkbox', link: 'components/checkbox.md' },
+          { text: 'Select', link: 'components/select.md' },
+          { text: 'Menu', link: 'components/menu.md' }
         ]
       },
-      { text: 'About', link: 'about.md' },
-      { text: 'Admin', link: 'https://vuepress.imst.xyz/admin/' }
+      { text: 'Sobre', link: 'about.md' }
     ],
 
     sidebar: 'auto',
     sidebar: [
       ['/', 'Home'],
-      ['docs/', 'Documents']
+      ['components/', 'Componentes']
     ],
 
     displayAllHeaders: true, // 默认值：false
@@ -39,7 +38,7 @@ module.exports = {
 
 
     // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: 'DemoMacro/VuePress-NetlifyCMS',
+    repo: 'harry-porto/VuePress-NetlifyCMS',
     // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
     // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
     repoLabel: 'Github',
@@ -55,6 +54,6 @@ module.exports = {
     // 默认是 false, 设置为 true 来启用
     editLinks: true,
     // 默认为 "Edit this page"
-    editLinkText: '帮助我们改善此页面！'
+    editLinkText: 'Editar'
   }
 }
